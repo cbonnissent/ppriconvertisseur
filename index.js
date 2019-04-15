f33bc86be6e35fdc1e4d2fcb26050d83c4c32e2e
Charles Bonnissent <charles.bonnissent@anakeen.com>
2019-04-15 21:17:31 +0200

    correct regexp

--- index.js
+++ index.js
@@ -63,7 +63,7 @@ function handleFiles() {
       if (!currentLine["conditionnement"]) {
         return acc;
       }
-      currentLine["conditionnement"] = currentLine["conditionnement"].replace(/\)$/, '');
+      currentLine["conditionnement"] = currentLine["conditionnement"].replace(/\)\)$/, '');
       return acc +
       "\n"+
       "<tr>"+
